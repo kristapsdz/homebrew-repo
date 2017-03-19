73d72ca6d5003c8e292a138951750e896fd302f2
kristaps <>
2017-03-19 09:23:11 +0000

    Update sblg to 0.4.1.

--- sblg.rb
+++ sblg.rb
@@ -1,8 +1,8 @@
 class Sblg < Formula
 	desc "simple off-line blog utility"
 	homepage "https://kristaps.bsd.lv/sblg"
-	url "https://kristaps.bsd.lv/sblg/snapshots/sblg-0.3.10.tar.gz"
-	sha256 "bd77dfc46e084994746039b2dd31859ae98e422d70ca2f09c5f3588525b58eea"
+	url "https://kristaps.bsd.lv/sblg/snapshots/sblg-0.4.1.tar.gz"
+	sha256 "cfdd88ef0ae3b5afdb72218c2f483eb79dbb7e06da0a9d51ca69eb584aecdae5"
 	def install
 		system "make", "install", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{pkgshare}", "BINDIR=#{bin}"
 	end
