b46b40784be9c0a29f90f20a7571f4848b79d302
kristapsdz <kristaps@bsd.lv>
2017-06-18 10:22:29 +0000

    Add missing configure and update.  From https://github.com/kristapsdz/homebrew-repo/pull/1 --- thanks!

--- sblg.rb
+++ sblg.rb
@@ -1,9 +1,10 @@
 class Sblg < Formula
 	desc "simple off-line blog utility"
 	homepage "https://kristaps.bsd.lv/sblg"
-	url "https://kristaps.bsd.lv/sblg/snapshots/sblg-0.4.1.tar.gz"
-	sha256 "cfdd88ef0ae3b5afdb72218c2f483eb79dbb7e06da0a9d51ca69eb584aecdae5"
+	url "https://kristaps.bsd.lv/sblg/snapshots/sblg-0.4.2.tar.gz"
+	sha256 "0d3a6318c3080065cfaecb3e662c8fbd43c6f4196fa6576b57e96e536f64dcdc"
 	def install
+		system "./configure"
 		system "make", "install", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{pkgshare}", "BINDIR=#{bin}"
 	end
 end
