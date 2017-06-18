0cd23faed4d17021cbb6b50c5511280eee376ab3
kristapsdz <kristaps@bsd.lv>
2017-06-18 10:22:38 +0000

    Update.

--- lowdown.rb
+++ lowdown.rb
@@ -1,8 +1,8 @@
 class Lowdown < Formula
   desc "simple Markdown translator"
   homepage "https://kristaps.bsd.lv/lowdown"
-  url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.1.10.tar.gz"
-  sha256 "eef2fcb2f7779e36d0a609c0ec2eb089134057fcb1a26a93e99350a7d8c2d729"
+  url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.1.11.tar.gz"
+  sha256 "d7f6e5dc9dc195de4ec8ae0dee72f95733df5a520df1d0a8a3af385daf0e2d2c"
   def install
     system "./configure"
     system "make", "install", "BINDIR=#{bin}", "LIBDIR=#{lib}", "INCLUDEDIR=#{include}", "MANDIR=#{man}"
