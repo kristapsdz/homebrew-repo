732bd2aea774c6847a3a53b77793eb0e19cea4b4
kristapsdz <kristaps@bsd.lv>
2017-06-24 11:02:03 +0000

    Change sha.

--- divecmd.rb
+++ divecmd.rb
@@ -2,7 +2,7 @@ class Divecmd < Formula
 	desc "download and process dive computer data"
 	homepage "https://kristaps.bsd.lv/divecmd"
 	url "https://kristaps.bsd.lv/divecmd/snapshots/divecmd-0.0.10.tar.gz"
-	sha256 "275ad350f2f4877f02e58de069fbe13cbae48ae223bd527ce279f83c48f195fc"
+	sha256 "bd3f981c2d8888e1f17f11c11f72949710e71a2f657cef1e2691a7ac996e457d"
 	depends_on "libdivecomputer" => :build
 	depends_on "groff" => :run
 	depends_on "grap" => :run
