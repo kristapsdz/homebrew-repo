7b36c2824ccdf1a6d10fb3cbb3f93a583e3e79ac
kristapsdz <kristaps@bsd.lv>
2017-06-24 10:54:45 +0000

    Start updating with newest divecmd.

--- divecmd.rb
+++ divecmd.rb
@@ -1,11 +1,13 @@
 class Divecmd < Formula
 	desc "download and process dive computer data"
 	homepage "https://kristaps.bsd.lv/divecmd"
-	url "https://kristaps.bsd.lv/divecmd/snapshots/divecmd-0.0.9.tar.gz"
-	sha256 "93cf5e428a803726acbde8857dea562e7a4a4b5f466e58a5f73188980baaecdf"
+	url "https://kristaps.bsd.lv/divecmd/snapshots/divecmd-0.0.10.tar.gz"
+	sha256 "275ad350f2f4877f02e58de069fbe13cbae48ae223bd527ce279f83c48f195fc"
 	depends_on "libdivecomputer" => :build
+	depends_on "groff" => :run
+	depends_on "grap" => :run
 	def install
-		system "./configure"
-		system "make", "install", "CFLAGS=-I#{include}", "PREFIX=#{prefix}", "LDFLAGS=-L#{lib}", "MANDIR=#{man}", "BINDIR=#{bin}"
+		system "./configure", "CPPFLAGS=-I#{include}", "PREFIX=#{prefix}", "LDFLAGS=-L#{lib}", "MANDIR=#{man}"
+		system "make", "install", "GROFF=#{HOMEBREW_PREFIX}/bin/groff"
 	end
 end
