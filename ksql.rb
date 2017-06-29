c8731a499f0f870242aaf45c7621774dddcc0bde
kristapsdz <kristaps@bsd.lv>
2017-06-29 09:48:15 +0000

    Update hash.

--- ksql.rb
+++ ksql.rb
@@ -2,7 +2,7 @@ class Ksql < Formula
 	desc "yet another C SQLite wrapper"
 	homepage "https://kristaps.bsd.lv/ksql"
 	url "https://kristaps.bsd.lv/ksql/snapshots/ksql-0.1.1.tar.gz"
-	sha256 "3156764970eb93ea64ee3c82e47e1875554a0092033684aa41f35c01d0efbe52"
+	sha256 "168c2002b1dbb66c1a5b0a248cf492313d37e413e5eb8ff1b7fb8c160b900471"
 	depends_on "sqlite" => :build
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "LIBDIR=#{lib}", "MANDIR=#{man}", "INCLUDEDIR=#{include}"
