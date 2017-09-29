4a6e7259e8e48a75a998e3cf736f426a12394e4b
kristapsdz <kristaps@bsd.lv>
2017-09-29 00:30:36 +0000

    Fix hash.

--- lowdown.rb
+++ lowdown.rb
@@ -2,7 +2,7 @@ class Lowdown < Formula
   desc "simple Markdown translator"
   homepage "https://kristaps.bsd.lv/lowdown"
   url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.2.6.tar.gz"
-  sha256 "173febbb750e8944972c3e7b67d86c9b4d5b69d63362f30835db279aa72a4518"
+  sha256 "ad9efa2fb55256927faf3c15506a9b3a26fd168ddde684217fce23ef79c81ea9"
   def install
     system "./configure"
     system "make", "install", "BINDIR=#{bin}", "LIBDIR=#{lib}", "INCLUDEDIR=#{include}", "MANDIR=#{man}"
