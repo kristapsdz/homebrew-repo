8810419e8632b65cadeb05a479f5cd9e903cc2b5
kristapsdz <kristaps@bsd.lv>
2017-10-18 23:07:08 +0000

    Fixed, but will only work with next release.

--- kcgi.rb
+++ kcgi.rb
@@ -1,10 +1,10 @@
 class Kcgi < Formula
 	desc "minimal CGI and FastCGI library in C"
 	homepage "https://kristaps.bsd.lv/kcgi"
-	url "https://kristaps.bsd.lv/kcgi/snapshots/kcgi-0.9.8.tgz"
-	sha256 "13f48ab12304a51fef13459ae06177b8a41a2cebc4962616729d5afdce51328e"
+	url "https://kristaps.bsd.lv/kcgi/snapshots/kcgi-0.9.9.tgz"
+	sha256 ""
 	def install
-		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "LIBDIR=#{lib}", "MANDIR=#{man}", "INCLUDEDIR=#{include}", "DATADIR=#{pkgshare}", "SBINDIR=#{sbin}"
+		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "LIBDIR=#{lib}", "MANDIR=#{man}", "INCLUDEDIR=#{include}", "SHAREDIR=#{share}", "SBINDIR=#{sbin}"
 		system "make", "install"
 	end
 end
