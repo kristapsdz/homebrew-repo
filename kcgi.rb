1f2df9429af869394c2b82849c16e17d6075d794
kristapsdz <kristaps@bsd.lv>
2017-11-01 16:43:31 +0000

    Sync.

--- kcgi.rb
+++ kcgi.rb
@@ -2,7 +2,7 @@ class Kcgi < Formula
 	desc "minimal CGI and FastCGI library in C"
 	homepage "https://kristaps.bsd.lv/kcgi"
 	url "https://kristaps.bsd.lv/kcgi/snapshots/kcgi-0.9.9.tgz"
-	sha256 ""
+	sha256 "84a98ed6d0f2a8df682be5c8a2b47a7d95b7fdb598cc103f02880f520f901dc5"
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "LIBDIR=#{lib}", "MANDIR=#{man}", "INCLUDEDIR=#{include}", "SHAREDIR=#{share}", "SBINDIR=#{sbin}"
 		system "make", "install"
--- lowdown.rb
+++ lowdown.rb
@@ -1,8 +1,8 @@
 class Lowdown < Formula
 	desc "simple Markdown translator"
 	homepage "https://kristaps.bsd.lv/lowdown"
-	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.3.0.tar.gz"
-	sha256 "5495a299227e49241ba2415065c0a3f32ed3334834db7aa611d65c5d2cbd57e7"
+	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.3.1.tar.gz"
+	sha256 "9e3796c7f819a3d28a8e304eac60da62e98fa996cae4131da971d8deaea44766"
 	def install
 		system "./configure", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}", "BINDIR=#{bin}", "INCLUDEDIR=#{include}", "LIBDIR=#{lib}"
 		system "make", "install"
