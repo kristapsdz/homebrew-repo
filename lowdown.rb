12b1748c1a92b4555e6ed85c8a5f46afb7fa55aa
kristapsdz <kristaps@bsd.lv>
2018-06-18 15:18:50 +0000

    Depends on libdiff.

--- lowdown.rb
+++ lowdown.rb
@@ -1,8 +1,9 @@
 class Lowdown < Formula
 	desc "simple Markdown translator"
 	homepage "https://kristaps.bsd.lv/lowdown"
-	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.3.2.tar.gz"
-	sha256 "740cf6025c43ceaae9bcc1ffc3e94986aa62d65d91ef8173d444a1977c562094"
+	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.3.1.tar.gz"
+	sha256 "9e3796c7f819a3d28a8e304eac60da62e98fa996cae4131da971d8deaea44766"
+	depends_on "libdiff"
 	def install
 		system "./configure", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}", "BINDIR=#{bin}", "INCLUDEDIR=#{include}", "LIBDIR=#{lib}"
 		system "make", "install"
