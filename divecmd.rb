234940fd5a3385c0e62c28467a7b86cb4bf7c424
kristapsdz <kristaps@bsd.lv>
2018-06-19 07:59:18 +0000

    Remove deprecated ':run' interface.

--- divecmd.rb
+++ divecmd.rb
@@ -4,8 +4,8 @@ class Divecmd < Formula
 	url "https://kristaps.bsd.lv/divecmd/snapshots/divecmd-0.0.15.tar.gz"
 	sha256 "75bba62a3274d0f182f9b1956d18e66b9b09d520e6b3285afb1e98e3474245e1"
 	depends_on "libdivecomputer" => :build
-	depends_on "groff" => :run
-	depends_on "grap" => :run
+	depends_on "groff"
+	depends_on "grap"
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "PREFIX=#{prefix}", "LDFLAGS=-L#{lib}", "MANDIR=#{man}"
 		system "make", "install", "GROFF=#{HOMEBREW_PREFIX}/bin/groff"
--- lowdown.rb
+++ lowdown.rb
@@ -3,7 +3,7 @@ class Lowdown < Formula
 	homepage "https://kristaps.bsd.lv/lowdown"
 	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.4.0.tar.gz"
 	sha256 "d075ddb3c1fb9908d1b72c395429a0faebd3940f9c995e817eb2e93923c9ccaa"
-	depends_on "kristapsdz/repo/libdiff"
+	depends_on "kristapsdz/repo/libdiff" => :build
 	def install
 		system "./configure", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}", "BINDIR=#{bin}", "INCLUDEDIR=#{include}", "LIBDIR=#{lib}"
 		system "make", "install"
