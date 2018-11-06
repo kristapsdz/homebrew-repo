b29705a8228288a213a9273526878440875ac06d
kristapsdz <kristaps@bsd.lv>
2018-11-06 12:01:37 +0000

    Sync.

--- kwebapp.rb
+++ kwebapp.rb
@@ -1,8 +1,8 @@
 class Kwebapp < Formula
 	desc "web application source generator"
 	homepage "https://kristaps.bsd.lv/kwebapp"
-	url "https://kristaps.bsd.lv/kwebapp/snapshots/kwebapp-0.6.3.tar.gz"
-	sha256 "43d3000620429fe51b879befc37298a52eab28319edf0cf211c57510122b7812"
+	url "https://kristaps.bsd.lv/kwebapp/snapshots/kwebapp-0.6.5.tar.gz"
+	sha256 "e5c596af8109520d866f47a2f4033965d46aa7e625051482d3c4190b8a0fc8d2"
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}"
 		system "make", "install"
