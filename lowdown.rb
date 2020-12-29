3df5a1c74732240d5251dd4ff787a897b7a55cc9
kristapsdz <kristaps@bsd.lv>
2020-12-29 21:21:28 +0000

    Don't depend upon libdiff.

--- lowdown.rb
+++ lowdown.rb
@@ -3,7 +3,6 @@ class Lowdown < Formula
 	homepage "https://kristaps.bsd.lv/lowdown"
 	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.7.5.tar.gz"
 	sha256 "7ca5f791f04d49f78bc5af0bc8b1c3271ba40a4fc932ec6fb1c1bcb73dc4879f"
-	depends_on "kristapsdz/repo/libdiff" => :build
 	def install
 		system "./configure", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}", "BINDIR=#{bin}", "INCLUDEDIR=#{include}", "LIBDIR=#{lib}"
 		system "make", "install"
