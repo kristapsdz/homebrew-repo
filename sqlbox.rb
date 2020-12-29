f61679186eb65a4ed0529941eb93636b6d24c3ca
kristapsdz <kristaps@bsd.lv>
2020-12-29 21:19:05 +0000

    Fix hash.

--- sqlbox.rb
+++ sqlbox.rb
@@ -1,8 +1,8 @@
 class Sqlbox < Formula
 	desc "database access library"
 	homepage "https://kristaps.bsd.lv/sqlbox"
-	url "https://kristaps.bsd.lv/sqlbox/snapshots/sqlbox-0.3.5.tar.gz"
-	sha256 "8db29bf3af8a8af0b151efbcc0e9a908851e03b387601fda185cb06326501cb5"
+	url "https://kristaps.bsd.lv/sqlbox/snapshots/sqlbox-0.1.12.tar.gz"
+	sha256 "4bf3aca36754306f2a5dcdf90a8299273edecbfec9fe13e0d1358759fe7faab0"
 	depends_on "sqlite" => :build
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "LIBDIR=#{lib}", "MANDIR=#{man}", "INCLUDEDIR=#{include}"
