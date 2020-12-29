a94514d37eb9d3c127ccba7c6696f53781482dd1
kristapsdz <kristaps@bsd.lv>
2020-12-29 21:25:56 +0000

    Fix hashes.

--- openradtool.rb
+++ openradtool.rb
@@ -1,8 +1,8 @@
 class Openradtool < Formula
 	desc "application source generator"
 	homepage "https://kristaps.bsd.lv/openradtool"
-	url "https://kristaps.bsd.lv/openradtool/snapshots/openradtool-0.6.14.tar.gz"
-	sha256 "ea36334a143300055b507840a9ea190455f2ae914801d8c8847ff82ae194611e"
+	url "https://kristaps.bsd.lv/openradtool/snapshots/openradtool-0.11.9.tar.gz"
+	sha256 "31bc010ff42e2dc7c04e60efd2d2b229df3820980adce97068daec56f6686cab"
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}"
 		system "make", "install"
