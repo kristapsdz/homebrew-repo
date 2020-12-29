b17a57e1e7859ee310b4c524ac722f58fd8ffa48
kristapsdz <kristaps@bsd.lv>
2020-12-29 21:18:41 +0000

    Add sqlbox.

---
+++ sqlbox.rb
@@ -0,0 +1,11 @@
+class Sqlbox < Formula
+	desc "database access library"
+	homepage "https://kristaps.bsd.lv/sqlbox"
+	url "https://kristaps.bsd.lv/sqlbox/snapshots/sqlbox-0.3.5.tar.gz"
+	sha256 "8db29bf3af8a8af0b151efbcc0e9a908851e03b387601fda185cb06326501cb5"
+	depends_on "sqlite" => :build
+	def install
+		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "LIBDIR=#{lib}", "MANDIR=#{man}", "INCLUDEDIR=#{include}"
+		system "make", "install"
+	end
+end
