cb5136f5833d2c5543ca2c8843c283ec3f8f75fa
Mark Cornick <mark@markcornick.com>
2022-07-26 22:26:45 -0400

    lowdown 1.0.0

--- Formula/lowdown.rb
+++ Formula/lowdown.rb
@@ -1,8 +1,8 @@
 class Lowdown < Formula
 	desc "simple Markdown translator"
 	homepage "https://kristaps.bsd.lv/lowdown"
-	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.8.3.tar.gz"
-	sha256 "8cab86cc710c02fdbc61a7bd716fd896a44bdade2e16dc88afdc76ae0bb0ef2a"
+	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-1.0.0.tar.gz"
+	sha256 "a7c9dbbd40e3e9d9296ff6ef6ac54fec83b1abe54db7b6113cd9c0f692a20c13"
 	def install
 		system "./configure", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}", "BINDIR=#{bin}", "INCLUDEDIR=#{include}", "LIBDIR=#{lib}"
 		system "make", "install"
